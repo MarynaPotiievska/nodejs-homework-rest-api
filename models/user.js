f7b149a08d9ecdc3194c62f52b0b9db84d046d53
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-04-21 17:32:25 +0300

    created schema and model for user

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+
+const subscriptionList = ["starter", "pro", "business"];
+const emailRegexp = /^\w+([.-_]?\w+)*@\w+([.-_]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    min: 6,
+    required: [true, "Set password for user"],
+  },
+  email: {
+    type: String,
+    match: emailRegexp,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: subscriptionList,
+    default: "starter",
+  },
+  token: String,
+});
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  subscription: Joi.string().validate(...subscriptionList),
+  token: Joi.string(),
+});
+
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const schemas = { registerSchema, loginSchema };
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
