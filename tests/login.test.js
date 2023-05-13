c63127882e53ac883fa083840a3c7c4f0a37348c
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-05-13 15:16:43 +0300

    deleted comments

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,18 +2,14 @@ require("dotenv").config();
 
 const supertest = require("supertest");
 const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
 
 const app = require("../app");
-// const User = require("../models/user");
 
 const { DB_TEST_URI } = process.env;
 
 describe("test login controller", () => {
   beforeAll(async () => {
     await mongoose.connect(DB_TEST_URI);
-
-    // const hashedPassword = await bcrypt.hash("test123456", 10);
 
     await supertest(app).post("/api/users/register").send({
       password: "test123456",
@@ -30,8 +26,6 @@ describe("test login controller", () => {
       password: "test123456",
       email: "test@mail.com",
     });
-
-    // console.log(response);
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual(
